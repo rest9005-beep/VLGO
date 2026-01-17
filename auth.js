eebc8a4de2d77c8f87820e1e95133aaa3f0d3ccb
Gvatu <rest9005@gmail.com>
2026-01-17 09:55:00 +0300

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,5 @@
+// VLGO - Video Chat Application
+console.log('%cVLGO Video Chat', 'font-size: 24px; font-weight: bold; color: #6c63ff;');
+console.log('%cВерсия 1.0.0', 'color: #b0b0d0;');
+
+// ... остальной код auth.js остается таким же ...
